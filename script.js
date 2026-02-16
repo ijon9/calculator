39a5a9ea96f6337ed592523ddea807f366cdd3f6
Isaac Jon <isaac.jon72@gmail.com>
2026-02-15 22:47:17 -0500

    Added basic functionality

--- script.js
+++ script.js
@@ -2,3 +2,86 @@ let first = "";
 let operator = "";
 let second = "";
 let previous = "";
+
+function operate(first, operator, second) {
+    if(operator === "+") {
+        const res = +first + +second;
+        return Number(res.toFixed(5));
+    }
+    else if(operator === "-") {
+        const res = +first - +second;
+        return Number(res.toFixed(5));
+    }
+    else if(operator === "x") {
+        const res = +first * +second;
+        return Number(res.toFixed(5));
+    }
+    else if(operator === "÷") {
+        if(second === '0') throw "Divide by zero";
+        const res = +first / +second;
+        return Number(res.toFixed(5));
+    }
+    else return "";
+}
+
+function keyPress(key) {
+    // Key pressed is a number
+    if(typeof(key) === "number") {
+        // If first number is not chosen
+        if(operator === "") {
+            first += key;
+            previous = '';
+        }
+        else second += key;
+        displayInfo(first + operator + second);
+    }
+    // Key pressed is equal
+    else if(key === "=") {
+        // Only compute result if all slots are chosen
+        if(first !== '' && operator !== '' && second !=='') {
+            try {
+                previous = '' + operate(first, operator, second);
+                displayInfo(previous);
+            } catch (error) { // Divide by zero
+                displayInfo(error);
+                first = operator = second = previous = '';
+            }
+        }
+    }
+    // Key pressed is clear
+    else if(key === "clear") {
+        first = second = operator = previous = "";
+        displayInfo('');
+    }
+    // Key pressed is operator
+    else {
+        // First has a number but second doesn't
+        if(first !== '' && second === '') {
+            operator = key;
+            displayInfo(first + operator + second);
+        }
+        // Both first, operator and second are chosen
+        else if(first !== '' && second !== '') {
+            try {
+                first = '' + operate(first, operator, second);
+                second = '';
+                operator = key;
+                displayInfo(first + operator + second);
+            } catch(error) { // Divide by zero
+                displayInfo(error);
+                first = operator = second = previous = '';
+            }
+        }
+        // No first or second but there was a previous result
+        else if(previous !== '') {
+            first = previous;
+            operator = key;
+            displayInfo(first + operator + second);
+        }
+    }
+}
+
+function displayInfo(str) {
+    const d = document.querySelector("#display");
+    d.textContent = str;
+}
